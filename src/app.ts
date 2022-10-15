5c37df246ca04d2278887eb3948a0635df76d816
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-10-15 20:56:14 +0200

    feat :added logger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,47 @@
 import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+import postgresDdClient from "./utils/connect";
+import logger from "../src/utils/logger"
 
 class App {
     public express: express.Application;
 
-    constructor(){
-        this.express = express()
-    }
+   
+
+    constructor() {
+        this.express = express();
+        this.setMiddleware();
+        // this.setRoutes();
+        this.connectToDB();
+        this.catchError();
+      }
+    
+      private setMiddleware(): void {
+        this.express.use(cors());
+        this.express.use(express.json());
+        this.express.use(morgan("dev"));
+      }
+    
+      private setRoutes(): void {
+        this.express.use("/api/v1", );
+      }
+    
+      private async connectToDB(): Promise<void> {
+        try {
+    
+          await postgresDdClient.authenticate()
+          await postgresDdClient.sync({ force: true });
+          logger.info("database connected successfully !!!");
+            
+        } catch (error) {
+          logger.info(error)
+            
+        }
+      
+      }
+    
+      private catchError(): void {}
 
 
 }
