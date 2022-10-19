8ae20e83247d2f067e1c13c9d3b602e904bf8921
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-10-19 10:53:54 +0200

    feat :done with tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  forceExit:true
+  forceExit:true,
+  clearMocks:true,
+  resetMocks:true,
+  restoreMocks:true
 };
--- test/user2.test.ts
+++ test/user2.test.ts
@@ -1,4 +1,62 @@
+import { describe, expect, test, it, jest } from "@jest/globals";
+import supertest from "supertest";
+import * as UserService from "../src/api/services/user.service";
+import Server from "../src/app"
+const app = new Server().express
+
 /**
- * notes 
- * 1.
+ * notes
+ * 1.just list all the things that you want to
+ *
  */
+
+const userInput = {
+  email: "test@email.com",
+  displayName: "King Pharoah",
+  password: "chicken",
+};
+
+const userPayload = {
+  userId: "1",
+  email: "test@email.com",
+  displayName: "King Pharoah",
+  password: "chicken",
+};
+describe("user", () => {
+  //user registration
+  describe("user registration", () => {
+    describe("given the username and password are valid", () => {
+      it("should return the user payload", async () => {
+        const createUserServiceMock = jest
+          .spyOn(UserService, "createUserService")
+          //@ts-ignore
+          .mockReturnValueOnce(userPayload);
+
+          const {statusCode , body} = await supertest(app).post(`/api/v1/user/`).send(userInput)
+
+          expect(statusCode).toBe(200);
+        //   expect(body).toEqual(userPayload);
+          expect(createUserServiceMock).toHaveBeenCalledWith(userInput)
+
+
+      });
+    });
+ 
+    describe("given the password do not match", () => {
+      it("should return 400", () => {});
+    });
+
+    describe("given the user servie throws", () => {
+      it("should return a 500", async () => {
+        const createUserServiceMock = jest
+          .spyOn(UserService, "createUserService")
+          //@ts-ignore
+          .mockRejectedValue("I cant move ....!!!")
+          const {statusCode } = await supertest(app).post(`/api/v1/user/`).send(userInput)
+
+          expect(statusCode).toBe(500)
+          expect(createUserServiceMock).toHaveBeenCalled()
+      });
+    });
+  });
+});
